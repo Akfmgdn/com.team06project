c2823f600d0dbf3598c8f73e72b8d269261b84a4
barborosbaran <barborosbaran42@gmail.com>
2023-03-15 11:06:59 +0100

    15.03 Baran/2

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -13,7 +13,7 @@ import org.junit.runner.RunWith;
         },
         features="src/test/resources/features",
         glue= "stepdefinitions",
-        tags="@wip1",
+        tags="@brn3",
         dryRun= false
 )
 
