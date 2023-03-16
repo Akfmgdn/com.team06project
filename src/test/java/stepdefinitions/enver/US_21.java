cb6fac2236719176c77df1c5f9a84e54e0440c63
nvrymk <enverymk@gmail.com>
2023-03-16 20:19:36 +0200

    16.03..2023 2019

--- src/test/java/stepdefinitions/enver/US_21.java
+++ src/test/java/stepdefinitions/enver/US_21.java
@@ -3,46 +3,80 @@ package stepdefinitions.enver;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
+import org.junit.Assert;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.interactions.Actions;
-import pages.CartPage;
-import pages.HomePage;
-import pages.LoginPage;
-import pages.MyWalletPage;
+import pages.*;
 import utilities.Driver;
 import utilities.ReusableMethods;
 
 public class US_21 {
-    HomePage homePage=new HomePage();
-    LoginPage loginPage=new LoginPage();
+    HomePage homePage = new HomePage();
+    LoginPage loginPage = new LoginPage();
     CartPage cartPage = new CartPage();
     MyWalletPage myWalletPage = new MyWalletPage();
+    RefundDisputePage refundDisputePage = new RefundDisputePage();
+
     Actions actions = new Actions(Driver.getDriver());
 
 
     @Given("Go to  main site")
-    public void goToMainSite() {Driver.getDriver().get("https://qa.trendlifebuy.com/");}
+    public void goToMainSite() {
+        Driver.getDriver().get("https://qa.trendlifebuy.com/");
+    }
 
 
     @And("Close newsletter subscription form")
     public void closeNewsletterSubscriptionForm() {
-        ReusableMethods.waitForVisibility(myWalletPage.newsletterSubscribeCloseButton,30);
+        ReusableMethods.waitForVisibility(myWalletPage.newsletterSubscribeCloseButton, 30);
         myWalletPage.newsletterSubscribeCloseButton.click();
 
     }
 
     @Then("Click on  login button")
-    public void clickOnLoginButton() {myWalletPage.loginButton.click();}
+    public void clickOnLoginButton() {
+        myWalletPage.loginButton.click();
+    }
 
     @Then("Pass in the credentials and click on submit button")
-    public void passInTheCredentialsAndClickOnSubmitButton() {myWalletPage.loginEmailBox.sendKeys("user133@trendlifebuy.com");
+    public void passInTheCredentialsAndClickOnSubmitButton() {
+        myWalletPage.loginEmailBox.sendKeys("user133@trendlifebuy.com");
         myWalletPage.loginPasswordBox.sendKeys("Trendlife114");
-        JavascriptExecutor jse=(JavascriptExecutor)Driver.getDriver();
-        jse.executeScript("arguments[0].click();",myWalletPage.signInButton);
+        JavascriptExecutor jse = (JavascriptExecutor) Driver.getDriver();
+        jse.executeScript("arguments[0].click();", myWalletPage.signInButton);
 
     }
 
     @And("Click the dashboard button")
-    public void clickTheDashboardButton() {myWalletPage.dashboardLink.click();}
-}
+    public void clickTheDashboardButton() {
+        myWalletPage.dashboardLink.click();
+    }
 
+    @Then("Click the Refund&Dispute link")
+    public void clickTheRefundDisputeLink() {
+        ReusableMethods.jsClick(refundDisputePage.refundDisputePagelink);
+        Assert.assertTrue(refundDisputePage.refundDisputePagelink.isDisplayed());
+
+
+    }
+
+    @And("Close browser")
+    public void closeBrowser() {
+        Driver.getDriver().quit();
+    }
+
+
+    @Then("it should be verified that the products for which the return process has been started are displayed in the RefundList.")
+    public void itShouldBeVerifiedThatTheProductsForWhichTheReturnProcessHasBeenStartedAreDisplayedInTheRefundList() {
+        Assert.assertTrue(refundDisputePage.iadeUrunText.isDisplayed());
+    }
+
+
+
+
+
+
+
+
+
+}
