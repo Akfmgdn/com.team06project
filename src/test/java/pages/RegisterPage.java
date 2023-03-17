c5ea60d5d36de10c6a4dd5b2c4038973df2f7f48
bugHunter <akfmgdn@gmail.com>
2023-03-17 13:55:26 +0300

    Akif 17.03.2023

--- src/test/java/pages/RegisterPage.java
+++ src/test/java/pages/RegisterPage.java
@@ -12,6 +12,36 @@ public class RegisterPage {
     @FindBy(xpath = "//h3[@class='m-0']")
     public WebElement singUpText;
 
+    @FindBy(xpath = "//input[@id='first_name']")
+    public WebElement registerFirstNamebox;
+
+    @FindBy(xpath = "//input[@id='last_name']")
+    public WebElement registerLastNamebox;
+
+    @FindBy(xpath = "//input[@id='email']")
+    public WebElement registerMailbox;
+
+    @FindBy(xpath = "//input[@id='password']")
+    public WebElement registerPasswordbox1;
+
+    @FindBy(xpath = "//input[@id='password-confirm']")
+    public WebElement registerPasswordbox2;
+
+    @FindBy(xpath = "//span[@class='checkmark mr_15']")
+    public WebElement registerbycheckbox;
+
+    @FindBy(xpath = "//button[@class='amaz_primary_btn style2 radius_5px  w-100 text-uppercase  text-center mb_25']")
+    public WebElement registerSingUpButton;
+
+    @FindBy(xpath = "//a[normalize-space()='Sign In']")
+    public WebElement registerSingInLink;
+
+    @FindBy(xpath = "//div[@class='toast-title']")
+    public WebElement registerSingUpOkPopUpLink;
+
+    @FindBy(xpath = "//h3[@class='m-0']")
+    public WebElement registerSingInText;
+
 
 
 
